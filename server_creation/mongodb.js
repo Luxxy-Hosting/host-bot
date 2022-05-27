2b4e7b1224a32d6863182f0e1d81bf5408243c1c
zachary#0842 <codingwithice3fire@gmail.com>
2022-05-27 13:19:24 -0400

    Update mongodb.js

--- server_creation/mongodb.js
+++ server_creation/mongodb.js
@@ -15,7 +15,7 @@ module.exports = (userID, serverName, location) => {
         "nest": 8,
         "egg": 24,
         "docker_image": "quay.io/parkervcp/pterodactyl-images:db_mongo-4",
-        "startup": "mongod --auth --fork --dbpath /home/container/mongodb/ --port ${SERVER_PORT} --bind_ip 0.0.0.0 --logpath /home/container/logs/mongo.log; until nc -z -v -w5 127.0.0.1 ${SERVER_PORT}; do echo 'Waiting for mongodb connection...'; sleep 5; done && mongo 127.0.0.1:${SERVER_PORT} && mongo --eval 'db.getSiblingDB('admin').shutdownServer()' 127.0.0.1:${SERVER_PORT}",
+        "startup": "mongod --fork --dbpath /home/container/mongodb/ --port ${SERVER_PORT} --bind_ip 0.0.0.0 --logpath /home/container/logs/mongo.log -f /home/container/mongod.conf; until nc -z -v -w5 127.0.0.1 ${SERVER_PORT}; do echo 'Waiting for mongodb connection...'; sleep 5; done && mongo --username ${MONGO_USER} --password ${MONGO_USER_PASS} --host 127.0.0.1:${SERVER_PORT} && mongo --eval 'db.getSiblingDB('admin').shutdownServer()' 127.0.0.1:${SERVER_PORT}",
         "limits": {
             "memory": 1024,
             "swap": 0,
