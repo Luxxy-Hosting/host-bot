e43c6a42df45c16b2812d0556dfe2ecc624e5756
zachary#0842 <codingwithice3fire@gmail.com>
2022-05-28 01:24:43 -0500

    Delete InvitedByInfo.js

--- models/InvitedByInfo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const InvitedByInfo = new mongoose.Schema({
-    ID: String,
-    tag: String,
-    id: String
-})
-
-module.exports = mongoose.model('InvitedByInfo', InvitedByInfo);
