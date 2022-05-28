733537ce5c43a7f925e65034707b538d7de41363
zachary#0842 <codingwithice3fire@gmail.com>
2022-05-28 01:35:21 -0500

    Update link.js

--- commands/user/link.js
+++ commands/user/link.js
@@ -1,13 +1,15 @@
 const Discord = require('discord.js');
+const userData = require('../../models/userData');
 module.exports = (client, message, args) => {
-    const userDB = userData.get(message.author.id)
-    if(!userDB) return message.reply(":x: You dont have an account created. type `!user new` to create one")
+    //const userDB = userData.get(message.author.id)
+    const userDataDB = await userData.findOne({ ID: message.author.id });
+    if(!userDataDB) return message.reply(":x: You dont have an account created. type `!user new` to create one")
     
     message.channel.send({embeds:[
         new Discord.MessageEmbed()
         .setColor(`GREEN`)
-        .addField(`Username:`, `${userDB.username}`)
-        .addField(`Link Date:`, `${userDB.linkDate}`)
-        .addField(`Link Time:`, `${userDB.linkTime}`)
+        .addField(`Username:`, `${userDataDB.username}`)
+        .addField(`Link Date:`, `${userDataDB.linkDate}`)
+        .addField(`Link Time:`, `${userDataDB.linkTime}`)
     ]})
 }
