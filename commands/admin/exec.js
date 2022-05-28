fb9029b055053e106b5d5263c832a52859a27ac3
Luxxy-GF <68338577+Luxxy-GF@users.noreply.github.com>
2022-05-28 18:41:49 +0000

    exec

---
+++ commands/admin/exec.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+module.exports = {
+    name: "exec",
+    category: "Owner",
+    description: "Execute a command",
+    run: async (client, message, args) => {
+        if (message.author.id === client.config.owner) {
+            exec(args.join(" "), (err, stdout, stderr) => {
+                if (err) {
+                    message.channel.send(err);
+                    return;
+                }
+                if (stderr) {
+                    message.channel.send(stderr);
+                    return;
+                }
+                message.channel.send({ content: `\`\`\`${stdout}\`\`\``  }).catch(err => {
+                    message.channel.send(`sus ${err}`);
+                });
+            });
+        } else {
+            message.reply('<a:whatisthis:951132055134162954>')
+        }
+    }
+}
