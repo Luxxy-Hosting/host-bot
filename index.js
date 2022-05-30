150a3360ed84f2875f7514a576daee80fa2c971b
zachary#0842 <codingwithice3fire@gmail.com>
2022-05-29 21:48:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require(`./config.json`);
 const db = require('quick.db')
 const userData = require('./models/userData');
 const mongoose = require('mongoose')
+const colors = require('colors')
 // const ServerCount = require('./models/FreeServerCount');
 
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES', 'DIRECT_MESSAGES', 'GUILD_PRESENCES', 'GUILD_BANS'], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
@@ -40,3 +41,29 @@ require(`./handlers/command_handler`)(client);
 if(config.settings.consoleSave) require(`./logs/console.log`)()
 
 client.login(config.bot.token);
+
+Anticrash script for your codes
+/*           ANTI CRASHING            ¦¦           ANTI CRASHING           */ 
+process.on('unhandledRejection', (reason, p) => {
+    console.log('\n\n\n\n\n[Anti-Crash] unhandled Rejection:'.toUpperCase().red.dim);
+    console.log(reason.stack ? String(reason.stack) : String(reason));
+    console.log('=== unhandled Rejection ===\n\n\n\n\n'.toUpperCase().red.dim);
+  });
+  process.on("uncaughtException", (err, origin) => {
+    console.log('\n\n\n\n\n\n[Anti-Crash] uncaught Exception'.toUpperCase().red.dim);
+    console.log(err.stack.yellow.dim ? err.stack.yellow.dim : err.yellow.dim)
+    console.log('=== uncaught Exception ===\n\n\n\n\n'.toUpperCase().red.dim);
+  })
+  process.on('uncaughtExceptionMonitor', (err, origin) => {
+    console.log('[Anti-Crash] uncaught Exception Monitor'.toUpperCase().red.dim);
+  });
+  process.on('exit', (code) => {
+    console.log('\n\n\n\n\n[Anti-Crash] exit'.toUpperCase().red.dim);
+    console.log(code.yellow.dim);
+    console.log('=== exit ===\n\n\n\n\n'.toUpperCase().red.dim);
+  });
+  process.on('multipleResolves', (type, promise, reason) => {
+    console.log('\n\n\n\n\n[Anti-Crash] multiple Resolves'.toUpperCase().red.dim);
+    console.log(type, promise, reason);
+    console.log('=== multiple Resolves ===\n\n\n\n\n'.toUpperCase().red.dim);
+  }); 
