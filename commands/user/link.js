948159748cd283485248dba6fb926da0acdabddb
zachary#0842 <codingwithice3fire@gmail.com>
2022-05-30 15:04:03 -0400

    Update link.js

--- commands/user/link.js
+++ commands/user/link.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const userData = require('../../models/userData');
+const config = require('../../config.json')
 module.exports = async (client, message, args) => {
     //const userDB = userData.get(message.author.id)
     const userDataDB = await userData.findOne({ ID: message.author.id });
-    if(!userDataDB) return message.reply(":x: You dont have an account created. type `!user new` to create one")
+    if(!userDataDB) return message.reply(`:x: You dont have an account created. type `\${config.bot.prefix}user new\` to create one`)
     
     message.channel.send({embeds:[
         new Discord.MessageEmbed()
