f749b1876434138b22067c39f04564124640943d
zachary#0842 <codingwithice3fire@gmail.com>
2022-05-30 16:11:23 -0400

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,3 +1,5 @@
+const config = require('../config.json')
+const Discord = require('discord.js')
 module.exports = (client, oldMessage, newMessage) => {
 
     if (oldMessage.author == null || oldMessage.author.bot == true || !oldMessage.content || newMessage == null) return;
@@ -14,4 +16,15 @@ module.exports = (client, oldMessage, newMessage) => {
 
     client.snipes.set(oldMessage.channel.id, client.snipes.get(oldMessage.channel.id).filter(x => (Date.now() - x.timestamp) < 300000 && x != null));
 
+    const embed = new Discord.MessageEmbed()
+    embed.setTitle('✏️ Edited Message')
+    embed.setColor(`BLUE`)
+    oldMessage.content ? embed.addField(`Message Content`, oldMessage.content.includes('```') ? `${oldMessage.content}` : `\`\`\`\n${oldMessage.content}\`\`\``) : null
+    oldMessage.attachments?.size !== 0 ? embed.setImage(oldMessage.attachments?.first()?.proxyURL) : null
+    oldMessage.attachments?.size !== 0 ? content = oldMessage.attachments?.map(x => x?.proxyURL).join("\n") : content = null
+    embed.setFooter({ text: `${oldMessage.member.user.tag} (${oldMessage.member.user.id}) \nin #${oldMessage.channel.name}`, iconURL: oldMessage?.member?.user?.displayAvatarURL()});
+    embed.setTimestamp()
+    
+
+    client.channels.cache.get(config.channelID.messageLog).send({content: content, embeds: [embed]}).catch(err => {})
 }
