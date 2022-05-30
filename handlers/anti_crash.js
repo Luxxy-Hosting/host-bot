bcbfd368e7935526ab97b8a6b66b719fd64497c9
Luxxy-GF <68338577+Luxxy-GF@users.noreply.github.com>
2022-05-30 10:09:55 +0000

    better anticrash handler

---
+++ handlers/anti_crash.js
@@ -0,0 +1,32 @@
+const chalk = require('chalk');
+const config = require('../config.json')
+module.exports = async (process) => {
+    if (config.settings.anitcrash) {
+        console.log(chalk.red('[Anti-Crash]') + ' is active ')
+
+    process.on('unhandledRejection', (reason, p) => {
+        console.log(chalk.red(`\n\n\n\n\n[Anti-Crash] unhandled Rejection:`));
+        console.log(reason.stack ? String(reason.stack) : String(reason));
+        console.log('=== unhandled Rejection ===\n\n\n\n\n'.toUpperCase().red.dim);
+    });
+    process.on("uncaughtException", (err, origin) => {
+        console.log(chalk.red(`\n\n\n\n\n\n[Anti-Crash] uncaught Exception`));
+        console.log(origin);
+        console.log(chalk.red('=== uncaught Exception ===\n\n\n\n\n'));
+    })
+    process.on('uncaughtExceptionMonitor', (err, origin) => {
+          console.log(chalk.red('[Anti-Crash] uncaught Exception Monitor'))
+    });
+    process.on('exit', (code) => {
+        console.log(chalk.red('\n\n\n\n\n[Anti-Crash] exit'));
+        console.log(code);
+        console.log(chalk.red('=== exit ===\n\n\n\n\n'));
+    });
+    process.on('multipleResolves', (type, promise, reason) => {
+        console.log(chalk.red('\n\n\n\n\n[Anti-Crash] multiple Resolves'));
+        console.log(type, promise, reason);
+        console.log(chalk.red('=== multiple Resolves ===\n\n\n\n\n'));
+    });
+} else {
+    console.log(chalk.red('[Anti-Crash]') + ' is inactive ')
+}}
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const config = require(`./config.json`);
 const db = require('quick.db')
 const userData = require('./models/userData');
 const mongoose = require('mongoose')
-const colors = require('colors')
 // const ServerCount = require('./models/FreeServerCount');
 
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES', 'DIRECT_MESSAGES', 'GUILD_PRESENCES', 'GUILD_BANS'], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
@@ -37,33 +36,8 @@ global.success = "<:yes:964979709945470977>"
 require("./handlers/music")(client);
 require(`./handlers/event_handler`)(client);
 require(`./handlers/command_handler`)(client);
+require(`./handlers/anti_crash`)(process);
 
 if(config.settings.consoleSave) require(`./logs/console.log`)()
 
 client.login(config.bot.token);
-
-Anticrash script for your codes
-/*           ANTI CRASHING            ¦¦           ANTI CRASHING           */ 
-process.on('unhandledRejection', (reason, p) => {
-    console.log('\n\n\n\n\n[Anti-Crash] unhandled Rejection:'.toUpperCase().red.dim);
-    console.log(reason.stack ? String(reason.stack) : String(reason));
-    console.log('=== unhandled Rejection ===\n\n\n\n\n'.toUpperCase().red.dim);
-  });
-  process.on("uncaughtException", (err, origin) => {
-    console.log('\n\n\n\n\n\n[Anti-Crash] uncaught Exception'.toUpperCase().red.dim);
-    console.log(err.stack.yellow.dim ? err.stack.yellow.dim : err.yellow.dim)
-    console.log('=== uncaught Exception ===\n\n\n\n\n'.toUpperCase().red.dim);
-  })
-  process.on('uncaughtExceptionMonitor', (err, origin) => {
-    console.log('[Anti-Crash] uncaught Exception Monitor'.toUpperCase().red.dim);
-  });
-  process.on('exit', (code) => {
-    console.log('\n\n\n\n\n[Anti-Crash] exit'.toUpperCase().red.dim);
-    console.log(code.yellow.dim);
-    console.log('=== exit ===\n\n\n\n\n'.toUpperCase().red.dim);
-  });
-  process.on('multipleResolves', (type, promise, reason) => {
-    console.log('\n\n\n\n\n[Anti-Crash] multiple Resolves'.toUpperCase().red.dim);
-    console.log(type, promise, reason);
-    console.log('=== multiple Resolves ===\n\n\n\n\n'.toUpperCase().red.dim);
-  }); 
