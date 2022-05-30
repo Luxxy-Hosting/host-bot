9b8b17c38585e5003ce0d9c0ae8587585d48be25
zachary#0842 <codingwithice3fire@gmail.com>
2022-05-30 16:15:32 -0400

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -15,16 +15,18 @@ module.exports = (client, oldMessage, newMessage) => {
     else client.snipes.set(oldMessage.channel.id, [...client.snipes.get(oldMessage.channel.id), data]);
 
     client.snipes.set(oldMessage.channel.id, client.snipes.get(oldMessage.channel.id).filter(x => (Date.now() - x.timestamp) < 300000 && x != null));
-
-    const embed = new Discord.MessageEmbed()
-    embed.setTitle('✏️ Edited Message')
-    embed.setColor(`BLUE`)
-    oldMessage.content ? embed.addField(`Message Content`, oldMessage.content.includes('```') ? `${oldMessage.content}` : `\`\`\`\n${oldMessage.content}\`\`\``) : null
-    oldMessage.attachments?.size !== 0 ? embed.setImage(oldMessage.attachments?.first()?.proxyURL) : null
-    oldMessage.attachments?.size !== 0 ? content = oldMessage.attachments?.map(x => x?.proxyURL).join("\n") : content = null
-    embed.setFooter({ text: `${oldMessage.member.user.tag} (${oldMessage.member.user.id}) \nin #${oldMessage.channel.name}`, iconURL: oldMessage?.member?.user?.displayAvatarURL()});
-    embed.setTimestamp()
     
-
-    client.channels.cache.get(config.channelID.messageLog).send({content: content, embeds: [embed]}).catch(err => {})
+    if(config.settings.messageLog){
+        const embed = new Discord.MessageEmbed()
+        embed.setTitle('✏️ Edited Message')
+        embed.setColor(`BLUE`)
+        oldMessage.content ? embed.addField(`Message Content`, oldMessage.content.includes('```') ? `${oldMessage.content}` : `\`\`\`\n${oldMessage.content}\`\`\``) : null
+        oldMessage.attachments?.size !== 0 ? embed.setImage(oldMessage.attachments?.first()?.proxyURL) : null
+        oldMessage.attachments?.size !== 0 ? content = oldMessage.attachments?.map(x => x?.proxyURL).join("\n") : content = null
+        embed.setFooter({ text: `${oldMessage.member.user.tag} (${oldMessage.member.user.id}) \nin #${oldMessage.channel.name}`, iconURL: oldMessage?.member?.user?.displayAvatarURL()});
+        embed.setTimestamp()
+        
+        
+        client.channels.cache.get(config.channelID.messageLog).send({content: content, embeds: [embed]}).catch(err => {})
+    }
 }
