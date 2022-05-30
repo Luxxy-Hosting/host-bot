970fc24c3cbcb6b441254152d836a4e926af5ffb
zachary#0842 <codingwithice3fire@gmail.com>
2022-05-30 15:34:34 -0400

    Update help.js

--- commands/staff/help.js
+++ commands/staff/help.js
@@ -4,11 +4,18 @@ const fs = require('fs');
 module.exports = async (client, message, args) => {
     if (!message.member.roles.cache.has(config.roleID.support)) return message.channel.send('You do not have the required permissions to use this command.');
     const commands = fs.readdirSync('./commands/staff/');
+    const push = [];
+    for (let i = 0; i < commands.length; i++) {
+        let command = commands[i].split('.')[0];
+        push.push(command);
+    }
     message.reply({
         embeds: [
             new Discord.MessageEmbed()
-            .setDescription(` __**Staff Commands**__\n\n ${commands.map(x => `\`\`\`${x}\`\`\``).join('\n')}`)
+            .setTitle('__**Staff Commands**__')
+            .setDescription(`**${config.bot.prefix}${push.join(`, ${config.bot.prefix}`)}**`)
             .setColor(`GREEN`)
+            .setFooter(client.user.tag, client.user.displayAvatarURL({ dynamic: true }));
         ]
     })
 }
