3cccf96f01a96ffb5462136ea36638a5519bc66e
zachary#0842 <codingwithice3fire@gmail.com>
2022-05-30 15:19:48 -0400

    Update help.js

--- commands/server/help.js
+++ commands/server/help.js
@@ -4,6 +4,6 @@ module.exports = (client, message, args) => {
         new Discord.MessageEmbed()
         .setTitle(`❓ | Need help?`)
         .setColor(`RED`)
-        .setDescription(`\`!server count\` - shows how many server slots you have and used\n\`!server create\` - create a server\n\`!server delete\` - delete a server\n\`!server list\` - shows all your servers created\n\`!server status\` - allows you to interact with the server\n\`!server proxy\` - Proxy your server to your domain\n\`!server unproxy\` - Delete a proxy`)
+        .setDescription(`\`${config.bot.prefix}server count\` - shows how many server slots you have and used\n\`${config.bot.prefix}server create\` - create a server\n\`${config.bot.prefix}server delete\` - delete a server\n\`!${config.bot.prefix}server list\` - shows all your servers created\n\`${config.bot.prefix}server status\` - allows you to interact with the server\n\`${config.bot.prefix}server proxy\` - Proxy your server to your domain\n\`${config.bot.prefix}server unproxy\` - Delete a proxy`)
     ]})
 }
