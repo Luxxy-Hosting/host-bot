65913ad9e5e82d086927f87e77b46cda73c94163
zachary#0842 <codingwithice3fire@gmail.com>
2022-06-08 21:05:14 -0400

    Create anti_crash.js

---
+++ handlers/anti_crash.js
@@ -0,0 +1,29 @@
+const chalk = require('chalk');
+const config = require('../config.json')
+module.exports = async (process) => {
+        console.log(chalk.red('[Anti-Crash]') + ' is active ')
+
+    process.on('unhandledRejection', (reason, p) => {
+        console.log(chalk.red(`\n\n\n\n\n[Anti-Crash] unhandled Rejection:`));
+        console.log(reason.stack ? String(reason.stack) : String(reason));
+        console.log('=== unhandled Rejection ===\n\n\n\n\n'.toUpperCase().red.dim);
+    });
+    process.on("uncaughtException", (err, origin) => {
+        console.log(chalk.red(`\n\n\n\n\n\n[Anti-Crash] uncaught Exception`));
+        console.log(origin);
+        console.log(chalk.red('=== uncaught Exception ===\n\n\n\n\n'));
+    })
+    process.on('uncaughtExceptionMonitor', (err, origin) => {
+          console.log(chalk.red('[Anti-Crash] uncaught Exception Monitor'))
+    });
+    process.on('exit', (code) => {
+        console.log(chalk.red('\n\n\n\n\n[Anti-Crash] exit'));
+        console.log(code);
+        console.log(chalk.red('=== exit ===\n\n\n\n\n'));
+    });
+    process.on('multipleResolves', (type, promise, reason) => {
+        console.log(chalk.red('\n\n\n\n\n[Anti-Crash] multiple Resolves'));
+        console.log(type, promise, reason);
+        console.log(chalk.red('=== multiple Resolves ===\n\n\n\n\n'));
+    });
+}}
