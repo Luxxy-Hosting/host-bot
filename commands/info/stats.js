cbc972af23a4a9266f7942521008ed856fa0706f
zachary#0842 <codingwithice3fire@gmail.com>
2022-06-13 11:27:57 -0400

    Update stats.js

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -9,8 +9,8 @@ module.exports = {
         const node2ping = await ping.promise.probe('https://n2.luxxy.host:8080')
         const node1online = node1ping.alive
         const node2online = node2ping.alive
-        const node11 = node1online ? 'Online' : 'Offline'
-        const node22 = node2online ? 'Online' : 'Offline'
+        const node11 = node1online ? 'Offline' : 'Online'
+        const node22 = node2online ? 'Offline' : 'Online'
 
         const embed = new Discord.MessageEmbed()
         .setColor('#ff0000')
