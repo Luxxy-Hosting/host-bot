4c00768254e934b4fafab833b73adb658a5e7203
zachary#0842 <codingwithice3fire@gmail.com>
2022-06-15 01:43:00 -0700

    Update blacklist.js

--- commands/admin/blacklist.js
+++ commands/admin/blacklist.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const config = require('../../config.json');
+const db = require("quick.db");
 module.exports = {
     name: "blacklist",
     category: "Owner",
