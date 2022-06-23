88b24d2aed9014f0c5e5398710a914ef5a3a4f97
zachary#0842 <codingwithice3fire@gmail.com>
2022-06-22 17:28:58 -0700

    Update ban.js

--- commands/staff/ban.js
+++ commands/staff/ban.js
@@ -3,7 +3,7 @@ const config = require('../../config.json');
 
 module.exports = async (client, message, args) => {
     if (!message.member.roles.cache.has(config.roleID.admin)) return message.channel.send('You do not have the required permissions to use this command.');
-    const user = await message.mentions.users.first()
+    const user = message.mentions.members.filter(member=>member.guild.id==message.guild.id).first() || message.guild.members.cache.get(args[0] ? args[0] : ``) || await message.guild.members.fetch(args[0] ? args[0] : ``).catch(() => {}) || false;
     if (!user) return message.reply({
         embeds: [
             new Discord.MessageEmbed()
@@ -14,15 +14,27 @@ module.exports = async (client, message, args) => {
     if (!message.guild.members.cache.get(user.id)) return message.reply({
         embeds: [
             new Discord.MessageEmbed()
-            .setTitle(`:x: | ${user.username} is not in this server`)
+            .setTitle(`:x: | ${user.user.tag} is not in this server`)
             .setColor(`RED`)
         ]
     })
-    message.guild.members.cache.get(user.id).ban({ reason: `${message.author.tag} run command` }).then(e => {
+    const memberPosition = user.roles.highest.position;
+    const moderationPosition = message.member.roles.highest.position;
+    if (moderationPosition <= memberPosition)
+        return message.reply({embeds : [new MessageEmbed()
+                                        .setColor(`RED`)
+                                        .setTitle(`:x: | I cannot ban someone, who is above/equal you`)
+                                       ]});
+    let reason = args.slice(2).join(` `);
+    if (!reason) {
+        reason = `NO REASON`;
+    }
+    
+    message.guild.members.cache.get(user.id).ban({ reason: reason }).then(e => {
         message.channel.send({
             embeds: [
                 new Discord.MessageEmbed()
-                .setTitle(`:white_check_mark: | ${user.username} has been banned`)
+                .setTitle(`:white_check_mark: | ${user.user.tag} has been banned for: ` + reason)
                 .setColor(`GREEN`)
             ]
         })
@@ -30,7 +42,7 @@ module.exports = async (client, message, args) => {
         message.channel.send({
             embeds: [
                 new Discord.MessageEmbed()
-                .setTitle(`:x: | ${user.username} could not be banned`)
+                .setTitle(`:x: | ${user.user.tag} could not be banned`)
                 .setDescription(`${err}`)
                 .setColor(`RED`)
             ]
