c42afb6d7a0fb7c37346156db659a1e251b82ce3
Lux Seed <deniedaddicted@gmail.com>
2022-07-03 13:35:42 +0000

    fixed few

--- index.js
+++ index.js
@@ -34,3 +34,96 @@ handler.loadSlashCommands(client);
 if(config.settings.consoleSave) require(`./logs/console.log`)()
 
 client.login(config.bot.token);
+
+
+const UserData1 = require('./models/userData.js')
+const { default: axios } = require('axios')
+client.on('guildMemberRemove', async member => {
+    const UserDB = await UserData1.findOne({ ID: member.id })
+    if (!UserDB) {
+        console.log('User is not in the database.')
+        client.channels.cache.get('942502078172000266').send('🤵 User is not in the database. (mongoose)')
+    }
+    if (UserDB) {
+        await axios({
+            url: config.pterodactyl.host + "/api/application/users/" + UserDB.consoleID + "?include=servers",
+            method: 'GET',
+            followRedirect: true,
+            maxRedirects: 5,
+            headers: {
+                'Authorization': 'Bearer ' + config.pterodactyl.adminApiKey,
+                'Content-Type': 'application/json',
+                'Accept': 'Application/vnd.pterodactyl.v1+json',
+            }
+        }).then(async res => {
+            const servers1 = res.data.attributes.relationships.servers.data.map(e => e.attributes.id)
+            client.channels.cache.get('942502078172000266').send({
+                embeds: [
+                    new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setDescription(` 🦺 ${member.user.tag} has left with consoleid ${UserDB.consoleID} with ${servers1.length} servers.`)
+                ]
+            })
+            client.channels.cache.get('942502078172000266').send('deleting.....')
+            if (servers1.length > 0) {
+                await client.channels.cache.get('942502078172000266').send({
+                    embeds: [
+                        new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle('🦺 Deleting servers...')
+                    ]
+                })
+                await Promise.all(servers1.map(async server => {
+                    client.channels.cache.get('942502078172000266').send({ embeds: [
+                        new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle(`🦺 Deleting server ${server}...`)
+                    ]})
+                    await axios({
+                        url: config.pterodactyl.host + "/api/application/servers/" + server + "/force",
+                        method: 'DELETE',
+                        followRedirect: true,
+                        maxRedirects: 5,
+                        headers: {
+                            'Authorization': 'Bearer ' + config.pterodactyl.adminApiKey,
+                            'Content-Type': 'application/json',
+                            'Accept': 'Application/vnd.pterodactyl.v1+json',
+                        }
+                    }).then(async res => {
+                        client.channels.cache.get('942502078172000266').send({ embeds: [
+                            new Discord.MessageEmbed()
+                            .setColor('#0099ff')
+                            .setTitle(`🦺 Server ${server} deleted.`)
+                        ]})
+                    }
+                    ).catch(err => {
+                        client.channels.cache.get('942502078172000266').send({ embeds: [
+                            new Discord.MessageEmbed()
+                            .setColor('#0099ff')
+                            .setTitle(`🦺 Error deleting server ${server}`)
+                        ]})
+                    }
+                    )
+                }))
+            }
+            await UserData1.deleteOne({ ID: member.id })
+            await axios({
+                url: config.pterodactyl.host + "/api/application/users/" + UserDB.consoleID,
+                method: 'DELETE',
+                followRedirect: true,
+                maxRedirects: 5,
+                headers: {
+                    'Authorization': 'Bearer ' + config.pterodactyl.adminApiKey,
+                    'Content-Type': 'application/json',
+                    'Accept': 'Application/vnd.pterodactyl.v1+json',
+                }
+            }).then(async res => {
+                client.channels.cache.get('942502078172000266').send({ embeds: [
+                    new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle(`🦺 User ${UserDB.consoleID} deleted.`)
+                ]})
+            })
+        })
+    }
+})
