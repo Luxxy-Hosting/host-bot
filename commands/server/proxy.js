1cd0538ff0c1f97969127dd772deb818d330ec8c
zachary#0842 <codingwithice3fire@gmail.com>
2022-07-05 10:42:26 -0500

    Update proxy.js

--- commands/server/proxy.js
+++ commands/server/proxy.js
@@ -3,7 +3,6 @@ const config = require('../../config.json')
 const axios = require('axios')
 const { getDomainIP, proxyDomain, findProxy, deleteProxy } = require(`../../nginxPM/index`)
 module.exports = async (client, message, args) => {
-    message.delete()
     let port
     let localdomain
     let serverid = args[1]
