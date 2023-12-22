b5297235f9f02bfd8ca902d9fcfa2cad8677eae3
Luxxy-GF <deniedaddicted@gmail.com>
2023-12-22 06:55:45 +0000

    update to the support commands

--- commands/staff/ban.js
+++ commands/staff/ban.js
@@ -6,28 +6,27 @@ const {
 } = require(`discord.js`);
 
 module.exports = async (client, message, args) => {
-    return message.reply('This command is currently disabled.');
     if (!message.member.roles.cache.has(config.roleID.admin)) return message.channel.send('You do not have the required permissions to use this command.');
     const user = message.mentions.members.filter(member=>member.guild.id==message.guild.id).first() || message.guild.members.cache.get(args[0] ? args[0] : ``) || await message.guild.members.fetch(args[0] ? args[0] : ``).catch(() => {}) || false;
     if (!user) return message.reply({
         embeds: [
             new Discord.EmbedBuilder()
             .setTitle(`:x: | You need to mention a user`)
-            .setColor(`RED`)
+            .setColor(Discord.Colors.Red)
         ]
     })
     if (!message.guild.members.cache.get(user.id)) return message.reply({
         embeds: [
             new Discord.EmbedBuilder()
             .setTitle(`:x: | ${user.user.tag} is not in this server`)
-            .setColor(`RED`)
+            .setColor(Discord.Colors.Red)
         ]
     })
     const memberPosition = user.roles.highest.position;
     const moderationPosition = message.member.roles.highest.position;
     if (moderationPosition <= memberPosition)
         return message.reply({embeds : [new EmbedBuilder()
-                                        .setColor(`RED`)
+                                        .setColor(Discord.Colors.Red)
                                         .setTitle(`:x: | I cannot ban someone, who is above/equal you`)
                                        ]});
     let reason = args.slice(2).join(` `);
@@ -40,7 +39,7 @@ module.exports = async (client, message, args) => {
             embeds: [
                 new Discord.EmbedBuilder()
                 .setTitle(`:white_check_mark: | ${user.user.tag} has been banned for: ` + reason)
-                .setColor(`GREEN`)
+                .setColor(Discord.Colors.Green)
             ]
         })
     }).catch(err => {
@@ -49,7 +48,7 @@ module.exports = async (client, message, args) => {
                 new Discord.EmbedBuilder()
                 .setTitle(`:x: | ${user.user.tag} could not be banned`)
                 .setDescription(`${err}`)
-                .setColor(`RED`)
+                .setColor(Discord.Colors.Green)
             ]
         })
     })
---
+++ commands/staff/finduser.js
@@ -0,0 +1,69 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+const axios = require('axios');
+const userData = require('../../models/userData');
+module.exports = async (client, message, args) => {
+    if (!message.member.roles.cache.has(config.roleID.support)) return message.channel.send('You do not have the required permissions to use this command.');
+    const email1 = await args[1]
+    const userDB = await userData.findOne({ email: email1 });
+    const servercount = serverCount.get(userDB.ID);
+    if (!userDB) return message.reply({
+        embeds: [
+            new Discord.EmbedBuilder()
+            .setTitle(`:x: | doesn't have an account yet`)
+            .setColor(Discord.Colors.Red)
+        ]
+    })
+    if (!servercount) return message.reply({
+        embeds: [
+            new Discord.EmbedBuilder()
+             .setTitle(`user don't have server count`)
+    ]})
+    axios({
+        url: config.pterodactyl.host + "/api/application/users/" + userDB.consoleID + "?include=servers",
+        method: 'GET',
+        followRedirect: true,
+        maxRedirects: 5,
+        headers: {
+
+            'Authorization': 'Bearer ' + config.pterodactyl.adminApiKey,
+            'Content-Type': 'application/json',
+            'Accept': 'Application/vnd.pterodactyl.v1+json',
+        },
+    }).then(async response => {
+        responce = response.data.attributes.relationships.servers.data;
+        let id = 1;
+        let id2 = 1;
+        axios({
+            url: config.pterodactyl.host + "/api/application/users/" + userDB.consoleID,
+            method: 'GET',
+            followRedirect: true,
+            maxRedirects: 5,
+            headers: {
+                'Authorization': 'Bearer ' + config.pterodactyl.adminApiKey,
+                'Content-Type': 'application/json',
+                'Accept': 'Application/vnd.pterodactyl.v1+json',
+            },
+        }).then(async res => {
+            message.reply({
+                embeds: [
+                    new Discord.EmbedBuilder()
+                    .addFields({ name: 'Console ID', value: `\`\`\`\n${userDB.consoleID}\`\`\``, inline: true })
+                    .addFields({ name: 'Email', value:  `\`\`\`\n${res.data.attributes.email}\`\`\``, inline: true })
+                    .addFields({ name: 'Discord ID', value: `\`\`\`\n${userDB.ID}\`\`\` <@!${userDB.ID}>`, inline: true })
+                    .addFields({ name: 'Username', value:  `\`\`\`\n${res.data.attributes.username}\`\`\``, inline: true })
+                    .addFields({ name: 'Link Date', value:  `\`\`\`\n${userDB.linkDate}\`\`\``, inline: true})
+                    .addFields({ name:'Link Time', value: `\`\`\`\n${userDB.linkTime}\`\`\``, inline: true})
+                    .addFields({ name: 'Servers', value:  `\`\`\`\n${responce.map(x => `${id++}. ${x.attributes.identifier}`).join('\n')}\`\`\``, inline: true})
+                    .addFields({ name: 'Servers Name', value:  `\`\`\`\n${responce.map(x => `${id2++}. ${x.attributes.name}`).join('\n')}\`\`\``, inline: true})
+                    .addFields({ name: 'Server Count', value: `\`\`\`\n${serverCount.get(userDB.ID).mineused} / ${serverCount.get(userDB.ID).minehave} \n${serverCount.get(userDB.ID).botused} / ${serverCount.get(userDB.ID).bothave}\`\`\``, inline: true})
+                    .setColor(Discord.Colors.Green)
+                ]
+            })
+        }).catch(err => {
+            console.log(err);
+        });
+    }).catch(err => {
+        console.log(err);
+    })
+}
